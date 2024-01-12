5a1c87089395f28d3874fea049e1e91443c20a42
stephenwelch1927 <stephenwelch1927@yahoo.com>
2024-01-12 14:09:48 +0700

    Added placeholders into contact page

--- src/components/Navbar/Contact.js
+++ src/components/Navbar/Contact.js
@@ -35,15 +35,15 @@ function Contact() {
         <form ref={form} onSubmit={sendEmail}>
           <label>Name: </label>
           <br></br>
-          <input type="text" name="from_name" />
+          <input type="text" placeholder="First Name, Surname eg Joe Smith" name="from_name" />
           <br></br>
           <label>Email: </label>
           <br></br>
-          <input type="email" name="from_email" />
+          <input type="email" placeholder="eg Joesmith@gmail.com" name="from_email" />
           <br></br>
           <label>Message: </label>
           <br></br>
-          <textarea name="message" />
+          <textarea placeholder="Your message .........." name="message" />
           <br></br>
           <br></br>
           <input type="submit" value="Send Message" />
