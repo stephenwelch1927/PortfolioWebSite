59faa2e964f0a5b0d49ca9d2d39184670300fdad
stephenwelch1927 <stephenwelch1927@yahoo.com>
2024-02-02 15:18:06 +0700

    Updated homepage position and images

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import Ayutthaya from '../images/Ayutthaya.jpg';
-import Salthill from '../images/Salthill_Winter1.jpg';
-import './styles/HomePage.css';
-import './styles/NavLayout.css';
-import ReactRoundedImage from 'react-rounded-image';
-  
+import React from "react";
+import Ayutthaya from "../images/Ayutthaya.jpg";
+import Salthill from "../images/Salthill_Winter1.jpg";
+import RoofTop from "../images/roofTop2.jpg";
+import "./styles/HomePage.css";
+import "./styles/NavLayout.css";
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="h1-style">Stephen Welch Web Development</h1>
-      <div>
-      <img src={Salthill} alt='Salthill' style={{height: "500px", width: "1900px" }} />
-      </div>
-      <div className='display'>
-       <ReactRoundedImage image={Ayutthaya} 
-        imageWidth="500"
-        imageHeight="500"
-        roundedColor="none"
-       />
-      </div>
-        
-      </div> 
+      <div id="frame">
+      <div id="main_image">
+      <img
+          style={{height: "600px", width: "1900px"}}
+          src={Salthill}
+          alt="Salthill"
+          
+        />
+        </div>
+        <div id="overlay-pos">
+
+        <img
+          id="overlay_image"
+          src={Ayutthaya}
+          alt="Ayutthaya"
+   
+        />
+
+        </div>
+        <br></br>
+        </div>
+        <div id="h1m">
+          <h1>Welcome to my Portfolio Site!</h1>
+        </div>
+
+    </div>
   );
 };
-  
+
 export default HomePage;
--- src/components/Navbar/Contact.js
+++ src/components/Navbar/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import "../styles/Contact.css";
+import '../styles/Contact.css';
 
 function Contact() {
   const form = useRef();
